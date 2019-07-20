d0cf9347f1db4352a9fab67f2fa77ccce80cd80f
kasif780780 <kasif780780@gmail.com>
2019-07-20 21:00:05 +0530

    first commit

---
+++ WebApplication11/Controllers/OrdersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class OrdersController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Orders
+        public ActionResult Index()
+        {
+            
+            List<Customer> OrderAndCustomerList = db.Customers.ToList();
+            return View(OrderAndCustomerList);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer order = db.Customers.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            List<Customer> OrderAndCustomerList = db.Customers.ToList();
+            return View(OrderAndCustomerList);
+        }
+
+
+        public ActionResult SaveOrder(string name, String address, Order[] order)
+        {
+            string result = "Error! Order Is Not Complete!";
+            if (name != null && address != null && order != null)
+            {
+                
+                Customer model = new Customer();
+                model.CustomerID = model.CustomerID;
+                model.Name = name;
+                model.Address = address;
+                model.OrderDate = DateTime.Now.ToString();
+                db.Customers.Add(model);
+
+                foreach (var item in order)
+                {
+                    
+                    Order O = new Order();
+                    O.OrderID = O.OrderID;
+                    O.ProductName = item.ProductName;
+                    O.Quantity = item.Quantity;
+                    O.Price = item.Price;
+                    O.Amount = item.Amount;
+                    O.CustomerID = item.CustomerID;
+                    db.Orders.Add(O);
+                }
+                db.SaveChanges();
+                result = "Success! Order Is Complete!";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
---
+++ WebApplication11/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication11.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string OrderDate { get; set; }
+        public string Address { get; set; }
+
+        public virtual IList<Order> Orders { get; set; }
+    }
+}
